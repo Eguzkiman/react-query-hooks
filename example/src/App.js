bce16e84cde055692c6c0ff8744632f0792db621
Eguzki <eaguirre@spoton.com>
2019-08-14 09:38:26 -0500

    added more examples

--- example/src/App.js
+++ example/src/App.js
@@ -10,6 +10,30 @@ const FETCH_USERS = ({ start = 0, limit = 3 } = {}) => {
 	);
 };
 
+function BasicExample () {
+	let users = useQuery(FETCH_USERS);
+
+	if (users.isLoading) return <Loading/>;
+	if (users.error) return <ErrorState error={users.error} onRetry={users.refetch}/>
+
+	return <List data={users.result.data}/>
+}
+
+function RefetchExample () {
+	let users = useQuery(FETCH_USERS);
+
+	if (users.isLoading || users.isReloading) return <Loading/>;
+	if (users.error) return <ErrorState error={users.error} onRetry={users.refetch}/>
+
+	return (
+		<div>
+			<List data={users.result.data}/>
+			<button onClick={users.refetch}>Fetch again</button>
+		</div>
+	);
+
+}
+
 function UseActionExample() {
 	let users = useAction(FETCH_USERS);
 	if (users.error) return <ErrorState error={users.error} />;
@@ -24,6 +48,28 @@ function UseActionExample() {
 		</div>
 	);
 }
+
+function DefaultPaginationExample () {
+	let users = useQuery(FETCH_USERS);
+
+	if (users.error) return <ErrorState error={users.error} onRetry={users.refetch} />;
+	if (users.isLoading) return <Loading />;
+
+	return (
+		<div>
+			<List data={users.result.data} />
+			{ users.isLoadingMore ? (
+				<p>Loading more...</p>
+			) : (
+				<button onClick={users.fetchMore}>Fetch More</button>
+			)}
+		</div>
+	);
+}
+
+// function CustomPaginationExample () {
+
+// }
 
 function ReloadExample() {
 	let [count, setCount] = useState(1);
@@ -82,6 +128,31 @@ function App() {
 					</div>
 				</div>
 			</section>
+			<section className="section">
+				<div className="container">
+					<h2 className="subtitle">Just fetch data</h2>
+					<p className="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit quae at, voluptatum dicta accusantium fugit nobis quisquam consequatur? Distinctio non ea delectus facere, asperiores optio iusto, at soluta qui placeat.</p>
+					<BasicExample />
+				</div>
+			</section>
+			<section className="section">
+				<div className="container">
+					<h2 className="subtitle">Refetching data</h2>
+					<RefetchExample />
+				</div>
+			</section>
+			<section className="section">
+				<div className="container">
+					<h2 className="subtitle">Pagination</h2>
+					<DefaultPaginationExample />
+				</div>
+			</section>
+			{/*<section className="section">
+				<div className="container">
+					<h2 className="subtitle">Custom Pagination</h2>
+					<CustomPaginationExample />
+				</div>
+			</section>*/}
 			<section className="section">
 				<div className="container">
 					<h2 className="subtitle">useAction</h2>
