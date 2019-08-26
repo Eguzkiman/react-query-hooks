e44ed61dd49e6eec403509daf450a37b354ef467
Eguzki <eaguirre@spoton.com>
2019-08-26 10:45:48 -0500

    rounded up for release

--- example/src/UserCard.js
+++ example/src/UserCard.js
@@ -1,26 +1,12 @@
 import React from 'react';
 
-const avatars = [
-	'https://avatarfiles.alphacoders.com/125/125425.png',
-	'https://avatarfiles.alphacoders.com/153/153748.png',
-	'https://avatarfiles.alphacoders.com/148/148965.png',
-	'https://avatarfiles.alphacoders.com/394/39465.jpg',
-	'https://avatarfiles.alphacoders.com/149/149743.jpg',
-	'https://avatarfiles.alphacoders.com/101/101478.jpg',
-	'https://avatarfiles.alphacoders.com/499/49919.jpg',
-	'https://avatarfiles.alphacoders.com/102/102796.png',
-	'https://avatarfiles.alphacoders.com/500/50046.jpg',
-	'https://avatarfiles.alphacoders.com/932/93245.png'
-]
-
-export default function UserCard ({ id, name, email }) {
-	let avatarUrl = avatars[id - 1];
+export default function UserCard ({ id, name, email, avatar }) {
 
 	return (
 		<div className="box media">
 			<div className="media-left">
 				<figure className="image is-64x64">
-					<img src={avatarUrl} className="is-rounded" alt={`${name}'s avatar`}/>
+					<img src={avatar} className="is-rounded" alt={`${name}'s avatar`}/>
 				</figure>
 			</div>
 			<div className="media-content">
--- example/src/components.js
+++ example/src/components.js
@@ -3,14 +3,14 @@ import React from 'react';
 import UserCard from './UserCard';
 
 export function Loading (props) {
-	return <p>Loading...</p>;
+	return <p className="content is-size-4">Loading...</p>;
 }
 
 export function ErrorState ({ error, onRetry }) {
 	return (
-		<div>
-			<p>Whoops! {String(error)}</p>
-			<button onClick={onRetry}>retry</button>
+		<div className="has-text-centered">
+			<p className="content is-size-4 has-text-danger">Whoopsey Daisy! {String(error)}</p>
+			<button className="button" onClick={onRetry}>Try again</button>
 		</div>
 	)
 }
